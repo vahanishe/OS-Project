68b361c27adb114468bb412f6b14413f539c3e68
Kitsune33 <kitsu.sjs@gmail.com>
2015-03-31 10:36:13 -0300

    Idem Anteriores, para Probar Notificacion por Mail

--- test_file.c
+++ test_file.c
@@ -5,5 +5,6 @@ void main() {
 	printf("\n\n\nProbemos con este archivito, Leo!!. \n"); 
 	printf("Esta es la línea que debe dar conflicto. \n");
 	printf("Otra Linea. \n");
+	printf("Otra Linea. \n");
 	printf("Prueba de Merge Error. \n\n\n");
 }
